587fb4cb04b09379af3862447c738a0dbf86c7ea
nogayhusrev <nogayhusrev@gmail.com>
2025-01-27 18:38:58 +0300

    overview done

--- src/test/java/iamhusrev/_1_starting_project/DemoUtilsTest.java
+++ src/test/java/iamhusrev/_1_starting_project/DemoUtilsTest.java
@@ -1,26 +1,41 @@
 package iamhusrev._1_starting_project;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.junit.jupiter.api.Assertions.assertLinesMatch;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNotSame;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.time.Duration;
+import java.util.List;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 
 //@DisplayNameGeneration(DisplayNameGenerator.Simple.class)
 //@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 //@DisplayNameGeneration(DisplayNameGenerator.Standard.class)
 //@DisplayNameGeneration(DisplayNameGenerator.IndicativeSentences.class)
+//@TestMethodOrder(MethodOrderer.DisplayName.class)
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+
 class DemoUtilsTest {
 
     DemoUtils demoUtils;
@@ -49,11 +64,20 @@ class DemoUtilsTest {
     }
 
     @Test
+    @Order(1)
     @DisplayName("Equals and Not Equals")
     void testEqualsAndNotEquals() {
         System.out.println("Running test: testEqualsAndNotEquals");
         assertEquals(6, demoUtils.add(2, 4), "2+4 must be 6");
         Assertions.assertNotEquals(6, demoUtils.add(1, 9), "1+9 must not be 6");
+    }
+
+    @Test
+    @Order(1)
+    @DisplayName("Equals and Not Equals")
+    void testMultiply() {
+        System.out.println("Running test: testMultiply");
+        assertEquals(6, demoUtils.multiply(2, 3), "2*4 must be 6");
     }
 
     @Test
@@ -84,6 +108,58 @@ class DemoUtilsTest {
         int gradeTwo = 5;
         assertTrue(demoUtils.isGreater(gradeOne, gradeTwo), "Object should be greater");
         assertFalse(demoUtils.isGreater(gradeTwo, gradeOne), "Object should not be greater");
+    }
+
+    @Test
+    @DisplayName("Array Equals")
+    void testArrayEquals() {
+        String[] stringArray = {"A", "B", "C"};
+        System.out.println("Running test: testArrayEquals");
+        assertArrayEquals(stringArray, demoUtils.getFirstThreeLettersOfAlphabet(), "Arrays should be the same");
+    }
+
+    @Test
+    @DisplayName("Iterable equals")
+    void testIterableEquals() {
+        List<String> theList = List.of("luv", "2", "code");
+
+        System.out.println("Running test: testIterableEquals");
+        assertIterableEquals(theList, demoUtils.getAcademyInList(), "Expected list should be same as actual list");
+    }
+
+    @Test
+    @DisplayName("Lines match")
+    void testLinesMatch() {
+        List<String> theList = List.of("luv", "2", "code");
+        System.out.println("Running test: testLinesMatch");
+        assertLinesMatch(theList, demoUtils.getAcademyInList(), "Lines should match");
+    }
+
+
+    @Test
+    @DisplayName("Throws and Does Not Throw")
+    void testThrowsAndDoesNotThrow() {
+        System.out.println("Running test: testThrowsAndDoesNotThrow");
+        assertThrows(Exception.class, () -> {
+            demoUtils.throwException(-1);
+        }, "Should throw exception");
+
+        System.out.println("Running test: testThrowsAndDoesNotThrow");
+        assertDoesNotThrow(() -> {
+            demoUtils.throwException(5);
+        }, "Should not throw exception");
+
+    }
+
+    @DisplayName("Timeout")
+    @Test
+    void testTimeout() {
+
+        System.out.println("Running test: testTimeout");
+        assertTimeoutPreemptively(Duration.ofSeconds(3), () -> {
+                demoUtils.checkTimeout();
+            },
+            "Method should execute in 3 seconds");
     }
 
 
