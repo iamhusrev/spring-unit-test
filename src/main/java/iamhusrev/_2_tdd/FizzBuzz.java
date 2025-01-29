7fedf1006ecd5cf945527624b45b4af09853b32d
nogayhusrev <nogayhusrev@gmail.com>
2025-01-29 11:50:55 +0300

    tdd

---
+++ src/main/java/iamhusrev/_2_tdd/FizzBuzz.java
@@ -0,0 +1,45 @@
+package iamhusrev._2_tdd;
+
+public class FizzBuzz {
+
+    // If number is divisible by 3, print Fizz
+    // If number is divisible by 5, print Buzz
+    // If number is divisible by 3 and 5, print FizzBuzz
+    // If number is NOT divisible by 3 or 5, then print the number
+
+    public static String compute(int i) {
+
+        StringBuilder result = new StringBuilder();
+
+        if (i % 3 == 0 && i % 5 == 0) {
+            result.append("FizzBuzz");
+        } else if (i % 3 == 0) {
+            result.append("Fizz");
+        } else if (i % 5 == 0) {
+            result.append("Buzz");
+
+        } else if (result.isEmpty()) {
+            result.append(i);
+        }
+
+        return result.toString();
+    }
+
+    /*
+    public static String compute(int i) {
+
+        if (i % 3 == 0 && i % 5 == 0) {
+            return "FizzBuzz";
+        } else if (i % 3 == 0) {
+            return "Fizz";
+        } else if (i % 5 == 0) {
+            return "Buzz";
+        } else {
+            return String.valueOf(i);
+        }
+
+    }
+
+    */
+
+}
